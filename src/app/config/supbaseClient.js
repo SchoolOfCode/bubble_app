5d942b07cc5507e8ab906e0ae2adb2de2cf305ed
chisfy <fyzac96@gmail.com>
2023-12-05 15:18:48 +0000

    supabase connected to emoji page, emojis now clickable

--- src/app/config/supbaseClient.js
+++ src/app/config/supbaseClient.js
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supapaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    supapaseURL,
+    supabaseAnonKey
 );
 
 export default supabase
--- src/app/emojis/page.js
+++ src/app/emojis/page.js
@@ -1,6 +1,7 @@
 "use client";
-import supabase from "../config/supbaseClient";
+import supabase from "../config/supbaseClient.js";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { SimpleGrid } from "@chakra-ui/react";
 import Image from "next/image";
 import Happy from "../../../public/assets/emojis/Happy.svg";
@@ -10,7 +11,25 @@ import Angry from "../../../public/assets/emojis/Angry.svg";
 import Worried from "../../../public/assets/emojis/Worried.svg";
 import Tired from "../../../public/assets/emojis/Tired.svg";
 
+
 export default function Emojis() {
+  
+  //trying to get the emoji to link to the next page
+  // const router = useRouter();
+  // router.push("../page.js");
+  
+  async function emojiClickHandler(emoji) {
+    console.log(emoji);
+    const { data, error } = await supabase.from("mood").insert([{ emoji }])
+
+    if (error){
+      console.log(error);
+    }
+    if (data) {
+      console.log("this has worked, check supabase");
+    }
+  }
+
   return (
     //set up a div to contain the text - "How are you feeling? (just pick one)
     //need to display the 6 emoji images in a grid - chakra ui grid
@@ -21,22 +40,22 @@ export default function Emojis() {
       </div>
       <SimpleGrid columns={2} spacing={10}>
         <div>
-          <Image src={Happy} alt="Happy" />
+          <Image src={Happy} alt="Happy-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Happy.svg")}/>
         </div>
         <div>
-          <Image src={Sad} alt="Sad" />
+          <Image src={Sad} alt="Sad-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Sad.svg")}/>
         </div>
         <div>
-          <Image src={Cheeky} alt="Cheeky" />
+          <Image src={Cheeky} alt="Cheeky-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Cheeky.svg")}/>
         </div>
         <div>
-          <Image src={Angry} alt="Angry" />
+          <Image src={Angry} alt="Angry-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Angry.svg")}/>
         </div>
         <div>
-          <Image src={Worried} alt="Worried" />
+          <Image src={Worried} alt="Worried-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Worried.svg")}/>
         </div>
         <div>
-          <Image src={Tired} alt="Tired" />
+          <Image src={Tired} alt="Tired-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Tired.svg")}/>
         </div>
       </SimpleGrid>
     </>
