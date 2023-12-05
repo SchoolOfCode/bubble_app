ae22baf740bc13868007852dd95f9e999b319f0d
carey-andrew <carey_andrew@hotmail.com>
2023-12-05 16:29:39 +0000

    links to next page

--- src/app/emojis/page.js
+++ src/app/emojis/page.js
@@ -1,7 +1,7 @@
 "use client";
 import supabase from "../config/supbaseClient.js";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useLink } from "next/router";
 import { SimpleGrid } from "@chakra-ui/react";
 import Image from "next/image";
 import Happy from "../../../public/assets/emojis/Happy.svg";
@@ -10,12 +10,13 @@ import Cheeky from "../../../public/assets/emojis/Cheeky.svg";
 import Angry from "../../../public/assets/emojis/Angry.svg";
 import Worried from "../../../public/assets/emojis/Worried.svg";
 import Tired from "../../../public/assets/emojis/Tired.svg";
+import Link from "next/link.js";
 
 
 export default function Emojis() {
   
   //trying to get the emoji to link to the next page
-  // const router = useRouter();
+//   const router = useRouter();
   // router.push("../page.js");
   //define the date 
     const date = new Date().toLocaleString();
@@ -30,6 +31,7 @@ export default function Emojis() {
     if (data) {
       console.log("this has worked, check supabase");
     }
+    // router.push("/");
   }
 
   return (
@@ -42,22 +44,34 @@ export default function Emojis() {
       </div>
       <SimpleGrid columns={2} spacing={10}>
         <div>
+            <Link href="/">
           <Image src={Happy} alt="Happy-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Happy.svg")}/>
+          </Link>
         </div>
         <div>
+        <Link href="/">
           <Image src={Sad} alt="Sad-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Sad.svg")}/>
+          </Link>
         </div>
         <div>
+        <Link href="/">
           <Image src={Cheeky} alt="Cheeky-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Cheeky.svg")}/>
+          </Link>
         </div>
         <div>
+            <Link href="/">
           <Image src={Angry} alt="Angry-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Angry.svg")}/>
+          </Link>
         </div>
         <div>
+        <Link href="/">
           <Image src={Worried} alt="Worried-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Worried.svg")}/>
+          </Link>
         </div>
         <div>
+        <Link href="/">
           <Image src={Tired} alt="Tired-Emoji" priority onClick={() => emojiClickHandler("../../../public/assets/emojis/Tired.svg")}/>
+          </Link>
         </div>
       </SimpleGrid>
     </>
