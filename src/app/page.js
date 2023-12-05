689037eea62bad6e6d317312f243858c7bf9aaea
India Grant <60491066+indiagrant@users.noreply.github.com>
2023-12-05 10:39:07 +0000

    Logo and emojis

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Logo from "/public/assets/BubbleLogo.svg";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Homepage</h1>
+      <Image src={Logo} alt="Logo" />
     </main>
   );
 }
