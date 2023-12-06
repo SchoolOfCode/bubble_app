79b85499fb047b49f9e406143fe4f08c6e6c2e48
chisfy <fyzac96@gmail.com>
2023-12-06 16:43:05 +0000

    relfections now can be added and inserted into table

--- src/app/reflection/page.jsx
+++ src/app/reflection/page.jsx
@@ -1,8 +1,11 @@
+"use client"
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Box, Button, Heading, Text, Textarea, Flex } from "@chakra-ui/react";
 import Footer from "../components/Footer";
-import FinishButton from "../components/FinishButton";
+import Textbox from "../components/Textbox";
+import supabase from "../config/supbaseClient"
+import useValue from "../hooks/useValue";
 
 export default function page() {
   return (
@@ -38,14 +41,7 @@ export default function page() {
         <Text>Press finished when you're done</Text>
       </Box>
       <Flex gap="10px" direction="column" alignItems="center">
-        <Textarea
-          bg="brand.blue"
-          maxWidth="600px"
-          placeholder="If you’re stuck, that’s okay start by writing about your day...what happened?"
-          border="none"
-          boxShadow="lg"
-        />
-
+        <Textbox />
         <Button bg="brand.yellow" maxW="600px" boxShadow="lg">
           See everything you jotted down
         </Button>
