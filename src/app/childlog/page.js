4f2ed1300c8a5738f302225618c2a0f09c960da7
Niklikescode <niklikescode@gmail.com>
2023-12-12 14:49:46 +0000

    toast childlog

--- src/app/childlog/page.js
+++ src/app/childlog/page.js
@@ -1,18 +1,22 @@
 "use client";
 import React from "react";
 import Navbar from "../components/Navbar";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, useToast, Flex, Text } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import ListOfLogs from "../components/ListOfLogs";
 import supabase from "../config/supabaseClient.js";
 import { useEffect, useState } from "react";
+
 
 //import the data from Supabase
 //store the data as a const
 //
 
 async function arrayOfLogs() {
+  
   const { data, error } = await supabase.from("mood").select();
+
+  
 
   if (error) {
     console.log(error);
@@ -25,10 +29,26 @@ async function arrayOfLogs() {
 }
 
 export default function page() {
+  const toast = useToast();
   const [logs, setLogs] = useState([]);
   useEffect(() => {
     arrayOfLogs().then(setLogs);
   }, []);
+
+    setTimeout(() => {toast({
+      position: "top",
+      // duration: 1000,
+      render: () => (
+        <Flex justifyContent="center" textAlign="center">
+          <Box color="white" p={3} bg="#4258A6" borderRadius="md">
+            <Text fontSize="lg" as="em">
+              Oops, nothing has been filled! If you&apos;re stuck just write
+              about a positive interaction with someone instead!
+            </Text>
+          </Box>
+        </Flex>
+      )})}, 2000);
+    
 
   return (
     <>
