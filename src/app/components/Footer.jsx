e7df3ec9bb201f0ddecbc8c65907b15687300c5d
Niklikescode <niklikescode@gmail.com>
2023-12-13 11:28:47 +0000

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       alignItems="center"
       textAlign="center"
       color="brand.footer"
-      position="sticky"
+      position="static"
       bottom="0"
       left="0"
       width="100%"
