e5214646ae8476bd44d833e4e52e8f67d82db370
chisfy <fyzac96@gmail.com>
2023-12-14 21:11:54 +0000

    height on page fixed

--- src/app/exercise/page.js
+++ src/app/exercise/page.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import style from '../breathing/page.module.css'
-import { Box, Button, Heading, Text, Flex, Link, Center, AbsoluteCenter } from '@chakra-ui/react';
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import style from "../breathing/page.module.css";
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  Flex,
+  Link,
+  Center,
+  AbsoluteCenter,
+} from "@chakra-ui/react";
 
 function page() {
-
   return (
     <>
       <Navbar />
       <Box
         width="80%"
-        height="70vh"
+        height="75vh"
         mx="auto"
         borderRadius="20px"
         textAlign="center"
@@ -21,15 +28,20 @@ function page() {
         mt="10"
         mb="5"
         boxShadow="lg"
+        css={{
+          "@media (max-height: 720px)": {
+            height: "70vh",
+          },
+        }}
       >
         <Heading paddingTop="3" as="h1" size="lg">
           Bubble Breathing!
         </Heading>
-      <Box className={style.bubble} position="relative" top="10"></Box>
+        <Box className={style.bubble} position="relative" top="10"></Box>
       </Box>
       <Footer />
     </>
   );
-};
+}
 
 export default page;
