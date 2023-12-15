06f1106b4654bcf0816f03c44c437f47cc5c4669
chisfy <fyzac96@gmail.com>
2023-12-15 14:04:53 +0000

    merged all work

--- src/app/exercise/page.js
+++ src/app/exercise/page.js
@@ -24,38 +24,6 @@ function page() {
     setAnimationComplete(true);
   };
 
-  // const [breathStage, setBreathStage] = useState("Breathe In");
-  // const [iterationCount, setIterationCount] = useState(0);
-  // const maxIterations = 2; 
-
-  // // useEffect(() => {
-  // //   const intervalId = setInterval(() => {
-  // //     setBreathStage((prevStage) => {
-  // //       switch (prevStage) {
-  // //         case "Breathe In":
-  // //           return "Hold";
-  // //         case "Hold":
-  // //           return "Breathe Out";
-  // //         case "Breathe Out":
-  // //           return "Hold";
-  // //         default:
-  // //           return "Breathe In";
-  // //       }
-  // //     });
-  // //   }, 8000); // Adjust the interval duration as needed
-
-  // //   // Clean up the interval when the component unmounts
-  // //   return () => clearInterval(intervalId);
-  // // }, []); 
-
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setBreathStage((prevStage) => (prevStage === "Breathe In" ? "Breathe Out" : "Breathe In"));
-  //   }, 8000); // Adjust the interval duration as needed
-
-  //   // Clean up the interval when the component unmounts
-  //   return () => clearInterval(intervalId);
-  // }, [maxIterations]);
  
 return (
     <>
@@ -65,7 +33,7 @@ return (
       <HeadingComponent />
       <Box
         width="80%"
-        height="75vh"
+        height="70vh"
         mx="auto"
         borderRadius="20px"
         textAlign="center"
@@ -75,13 +43,10 @@ return (
         boxShadow="lg"
         css={{
           "@media (max-height: 720px)": {
-            height: "65vh",
+            height: "60vh",
           },
         }}
       >
-        <Heading paddingTop="3" paddingBottom="5" as="h1" size={{ base: "xl", md: "xl", lg: "2xl" }}>
-          Bubble Breathing!
-        </Heading>
         <Center>
         <Flex
           justifyContent="center"
@@ -134,7 +99,7 @@ return (
       <HeadingComponent />
       <Box
         width="80%"
-        height="75vh"
+        height="70vh"
         mx="auto"
         borderRadius="20px"
         textAlign="center"
@@ -144,16 +109,10 @@ return (
         boxShadow="lg"
         css={{
           "@media (max-height: 720px)": {
-            height: "65vh",
+            height: "60vh",
           },
         }}
       >
-        {/* <Heading paddingTop="3" paddingBottom="5" as="h1" size={{ base: "xl", md: "xl", lg: "2xl" }}>
-          Bubble Breathing!
-        </Heading>
-            <Text className={style.breathingtext} fontSize={{ base: "xl", md: "4xl", lg: "4xl" }}>
-              Breathe In
-            </Text>
       <Box onAnimationEnd={handleAnimationEnd} className={style.bubble} position="relative">
       </Box>
         </Box>
@@ -161,5 +120,7 @@ return (
     </>
   )
 }
+</>
+)};
 
 export default page;
