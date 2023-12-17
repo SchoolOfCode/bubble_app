85bb4aa6fc015be67e78c8a29de8f9d8d4ff7e0e
chisfy <fyzac96@gmail.com>
2023-12-17 15:42:08 +0000

    new component added

--- src/app/components/BackgroundBox.jsx
+++ src/app/components/BackgroundBox.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React from "react";
+import { Box, Center, Flex } from "@chakra-ui/react";
 
 export default function BackgroundBox() {
   return (
-    <Box
-        width={[300, 400, 600]}
-        height={[325, 400, 400]}
-        mx="auto"
-        borderRadius="20px"
-        textAlign="center"
-        bg="brand.pink"
-        p="2"
-        mt="10"
-        boxShadow="lg"
-        zIndex={-1}
-        // css={{
-        //   "@media (max-height: 720px)": {
-        //     height: "60vh",
-        //   },
-        // }}
-      ></Box>
-  )
+        <Box
+          width={[300, 400, 600]}
+          height={[325, 400, 400]}
+          mx="auto"
+          borderRadius="20px"
+          textAlign="center"
+          bg="brand.pink"
+          p="2"
+          mt="10"
+          boxShadow="lg"
+          zIndex={-1}
+          // css={{
+          //   "@media (max-height: 720px)": {
+          //     height: "60vh",
+          //   },
+          // }}
+        ></Box>
+  );
 }
---
+++ src/app/components/BubbleBody.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Box, Flex, Text, Button, Center } from '@chakra-ui/react'
+import Link from 'next/link'
+
+export default function BubbleBody() {
+  return (
+    <Center>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          gap="20px"
+          position="absolute"
+          top="35%"
+        >
+          <Box
+            width={[300, 400, 600]}
+            bg="brand.yellow"
+            borderRadius="20px"
+            textAlign="center"
+            boxShadow="lg"
+            p="2"
+          >
+            <Text fontSize={{ base: "md", md: "lg", lg: "xl" }}>
+            Well done you are one step closer to feeling happier & calm! Why don't you record your mood now? Or better yet, talk to someone about your day!
+            </Text>
+          </Box>
+          <Button 
+          bg="brand.purple"
+          boxShadow="lg"
+          size="lg">
+            <Link href="/emojis" target="_self">
+              <Text fontSize={{ base: "md", md: "lg", lg: "xl" }}>
+                Record Mood
+              </Text>
+            </Link>
+          </Button>
+          <Button
+          bg="brand.blue"
+          boxShadow="lg"
+          size="lg">
+            <Link href="/exercise" target="_self">
+              <Text fontSize={{ base: "md", md: "lg", lg: "xl" }}>
+                Do it Again!
+              </Text>
+            </Link>
+          </Button>
+            </Flex>
+    </Center>
+  )
+}
