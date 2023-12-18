f70e128e0cb8d58f6349f8139ff56e34143b26fe
chisfy <fyzac96@gmail.com>
2023-12-18 16:08:27 +0000

    test chart made

--- src/app/parentcharts/page.js
+++ src/app/parentcharts/page.js
@@ -1,13 +1,14 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeadingComponent from "../components/HeadingComponent";
+import { TestChart } from "../components/TestChart";
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <HeadingComponent />
-
+      <TestChart />
       <Footer />
     </>
   );
