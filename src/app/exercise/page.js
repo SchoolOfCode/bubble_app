23b5e13ceb560866612b6761d34417d38a9ea415
chisfy <fyzac96@gmail.com>
2023-12-19 20:20:12 +0000

    bubble responsiveness attempt

--- src/app/exercise/page.js
+++ src/app/exercise/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -13,12 +13,15 @@ import {
   Link,
   Center,
   AbsoluteCenter,
+  useBreakpointValue
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import BackgroundBox from "../components/BackgroundBox";
 import BubbleBody from "../components/BubbleBody";
 
 function page() {
+
+  const bubbleSize = useBreakpointValue({ base: '350px', sm: '450px', md: '600px', lg: '600px' });
+
   const [animationComplete, setAnimationComplete] = useState(false);
 
   const handleAnimationEnd = () => {
@@ -26,48 +29,61 @@ function page() {
     setAnimationComplete(true);
   };
 
- 
-return (
+  return (
     <>
-    {animationComplete ? (
-      <>
-      <Navbar />
-      <HeadingComponent />
-      <BubbleBody />
-      <Footer />
-      </>
-    ) : (
-      <>
-      <Navbar />
-      <HeadingComponent />
-      {/* <BackgroundBox /> */}
-      <Box
-          width={[300, 400, 600]}
-          height={[325, 400, 510]}
-          mx="auto"
-          borderRadius="20px"
-          textAlign="center"
-          bg="brand.pink"
-          p="2"
-          mt="10"
-          boxShadow="lg"
-          zIndex={-1}
-          // css={{
-          //   "@media (max-height: 720px)": {
-          //     height: "60vh",
-          //   },
-          // }}
-        >
-      <Box className={style.textcontainer}>
-      </Box>
-      <Box onAnimationEnd={handleAnimationEnd} className={style.bubble} >
-      </Box>
-        </Box>
-      <Footer />
+      {animationComplete ? (
+        <>
+          <Navbar />
+          <HeadingComponent />
+          <BubbleBody />
+          <Footer />
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <HeadingComponent />
+          {/* <BackgroundBox /> */}
+          <Center>
+            {/* <Box
+              width={{ base: "80%" }}
+              height={{ base: "100%" }}
+              mx="auto"
+              borderRadius="20px"
+              textAlign="center"
+              bg="brand.pink"
+              p="2"
+              mt="10"
+              boxShadow="lg"
+              zIndex={-1}
+            > */}
+            <Box
+              borderRadius="20px"
+              textAlign="center"
+              bg="brand.pink"
+              p="2"
+              mt="10"
+              boxShadow="lg"
+              zIndex={-1}
+              width={bubbleSize}
+              height={bubbleSize}
+            >
+            <Flex flexDirection="column" textAlign="center" justifyContent="center">
+              <Box className={style.textcontainer}></Box>
+              <Center>
+                <Box
+                  onAnimationEnd={handleAnimationEnd}
+                  className={style.bubble}
+                ></Box>
+              </Center>
+            </Flex>
+            </Box>
+            {/* </Box> */}
+          </Center>
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
-</>
-)};
 
 export default page;
