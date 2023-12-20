bc7b9f6e38aab9a1adfa137ec24c73df1a3755d0
chisfy <fyzac96@gmail.com>
2023-12-20 19:54:40 +0000

    video sizing fixed

--- src/app/components/VideoPlayer.jsx
+++ src/app/components/VideoPlayer.jsx
@@ -1,23 +1,35 @@
 "use client";
 import React from "react";
 import ReactPlayer from "react-player";
-import { Box, Flex } from "@chakra-ui/react";
+import { AspectRatio, Box, Flex } from "@chakra-ui/react";
 
 const VideoPlayer = () => {
   return (
-    <div>
-      <Box zIndex={1} mb="20px">
-        <Flex justify="center" alignItems="center">
-          <ReactPlayer
-            url="https://youtu.be/FpUNhx1IFec"
-            controls
-            width="600px"
-            height="300px"
-          />
-        </Flex>
-      </Box>
-    </div>
+    <Flex justify="center" alignItems="center">
+    <Box zIndex={1} mb="20px" w={[300, 400, 600]} h={[200, 200, 350]}>
+      <ReactPlayer
+        url="https://youtu.be/FpUNhx1IFec"
+        controls
+        title="Bubble Breating Video"
+        width="100%"
+        height="100%"
+      />
+    </Box>
+    </Flex>
   );
+  //   <div>
+  //     <Box zIndex={1} mb="20px">
+  //       <Flex justify="center" alignItems="center">
+  //         <ReactPlayer
+  //           url="https://youtu.be/FpUNhx1IFec"
+  //           controls
+  //           width="600px"
+  //           height="300px"
+  //         />
+  //       </Flex>
+  //     </Box>
+  //   </div>
+  // );
 };
 
 export default VideoPlayer;
